f8b33322c592a09c47c5846b94b3c9864052e598
kilian1103 <kilian1103hotmail.de>
2025-06-28 21:33:30 +0100

    Updates

--- assets/all-wallets-B15ACHh0.js
+++
@@ -1,6 +0,0 @@
-import{b as o}from"./core-CvBNeieS.js";import"./index-p3oJy1T1.js";import"./events-CBsHrGTZ.js";import"./index.es-BctIwKYz.js";const r=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{r as allWalletsSvg};
